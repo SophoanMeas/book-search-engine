88141754d6d405dbf8c5d7b41ce30df1f4fbc0a7
Sophoan Meas <sophoan.meas@hotmail.com>
2022-07-17 16:51:28 -0400

    add savebook, searchbook pages

---
+++ client/src/utils/mutation.js
@@ -0,0 +1,86 @@
+import { gql } from '@apollo/client';
+
+// mutation --> user login
+export const LOGIN_USER = gql`
+  mutation loginUser($email: String!, $password: String!) {
+    login(email: $email, password: $password) {
+      token
+      user {
+        _id
+        username
+        email
+        bookCount
+        savedBooks {
+          bookId
+          title
+          description
+          authors
+          link
+          image
+        }
+      }
+    }
+  }
+`;
+
+// mutation --> add user
+export const ADD_USER = gql`
+  mutation addUser($username: String!, $email: String!, $password: String!) {
+    addUser(username: $username, email: $email, password: $password) {
+      token
+      user {
+        _id
+        username
+        email
+        bookCount
+        savedBooks {
+          bookId
+          title
+          description
+          Authors
+          image
+          link
+        }
+      }
+    }
+  }
+`;
+
+// mutation --> save books
+export const SAVE_BOOK = gql`
+  mutation saveBook($input: bookInput!) {
+    savedBook(input: $input) {
+      _id
+      username
+      email
+      savedBooks {
+        bookId
+        authors
+        image
+        description
+        title
+        link
+      }
+    }
+  }
+`;
+
+// mutaiton --> remove books
+export const REMOVE_BOOK = gql`
+  mutation removeBook($bookId: String!) {
+    removeBook(bookId: $bookId) {
+      _id
+      username
+      email
+      bookCount
+      savedBooks {
+        bookId
+        authors
+        image
+        description
+        title
+        link
+      }
+    }
+  }
+`;
