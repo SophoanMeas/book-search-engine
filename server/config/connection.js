b116a2913393e8b7523c7f5de60d91ebcee40440
Sophoan Meas <sophoan.meas@hotmail.com>
2022-07-17 23:39:44 -0400

    delop bug fix

--- server/config/connection.js
+++ server/config/connection.js
@@ -5,4 +5,7 @@ mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/book-search', {
   useUnifiedTopology: true,
 });
 
+// Use this to log mongo queries being executed!
+mongoose.set('debug', true);
+
 module.exports = mongoose.connection;
